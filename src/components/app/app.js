2e3da92f908fc7eb4158b7460114aab1efa5f65a
Anastasia Boshniak <boshniak.a@gmail.com>
2020-04-14 20:23:44 +0300

    Added App Component, node-sass package, base parameters

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,8 @@
 import React from "react";
+// import './_app.scss';
 
 const App = () =>{
-    return <div>Hello world</div>
+    return <div className='app'>Hello world</div>
 };
 
 export default App;
