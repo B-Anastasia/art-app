5e1dbd83e23b6246118f346fca83dff55c9b8021
Anastasia Boshniak <34798026+B-Anastasia@users.noreply.github.com>
2022-01-26 22:36:48 +0200

    Updates

---
+++ precache-manifest.e43743725768fa5d3d182c0164deeb89.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6ad2c8bfca5ef0d5373e070a9ae4b7",
+    "url": "/art-app/index.html"
+  },
+  {
+    "revision": "f0924f4534595ea5a987",
+    "url": "/art-app/static/css/main.c797c4dc.chunk.css"
+  },
+  {
+    "revision": "9c294b9292056e98c350",
+    "url": "/art-app/static/js/2.9e44c8ad.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/art-app/static/js/2.9e44c8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0924f4534595ea5a987",
+    "url": "/art-app/static/js/main.ef9108d4.chunk.js"
+  },
+  {
+    "revision": "2c72578943d63b6f7b20",
+    "url": "/art-app/static/js/runtime-main.721c031c.js"
+  },
+  {
+    "revision": "292c2094a58d8ee854e901cebae6a4c5",
+    "url": "/art-app/static/media/Cinzel Black.292c2094.ttf"
+  },
+  {
+    "revision": "83b91ef0c8821da6fddff2c026aa4f7a",
+    "url": "/art-app/static/media/Cinzel Black.83b91ef0.svg"
+  },
+  {
+    "revision": "8cbb7ae20de7757aa4810c847cb55e28",
+    "url": "/art-app/static/media/Cinzel Black.8cbb7ae2.woff"
+  },
+  {
+    "revision": "e469b95e5817272d350873d8372d9836",
+    "url": "/art-app/static/media/Cinzel Black.e469b95e.eot"
+  },
+  {
+    "revision": "f819c3a5098ccadd2ed9eb8b12354d5a",
+    "url": "/art-app/static/media/Cinzel Black.f819c3a5.woff2"
+  },
+  {
+    "revision": "4667c275878ae190f289675f30937fb2",
+    "url": "/art-app/static/media/Cinzel Bold.4667c275.woff"
+  },
+  {
+    "revision": "6f4e0519a7a0d37f3ca6c4857da23d9b",
+    "url": "/art-app/static/media/Cinzel Bold.6f4e0519.svg"
+  },
+  {
+    "revision": "84d0c78853415dce2bc824eb8161ecee",
+    "url": "/art-app/static/media/Cinzel Bold.84d0c788.ttf"
+  },
+  {
+    "revision": "c74015808ec2d835c6610260edbddfac",
+    "url": "/art-app/static/media/Cinzel Bold.c7401580.eot"
+  },
+  {
+    "revision": "c8a9c651879965e0b48c877d2f7ddcfd",
+    "url": "/art-app/static/media/Cinzel Bold.c8a9c651.woff2"
+  },
+  {
+    "revision": "316b16f280604bbf3b79c4fed84589ac",
+    "url": "/art-app/static/media/Cinzel Regular.316b16f2.woff"
+  },
+  {
+    "revision": "34674a1bf0e088d0aa45f38a09ab275b",
+    "url": "/art-app/static/media/Cinzel Regular.34674a1b.woff2"
+  },
+  {
+    "revision": "47b6e20bd1b2505a930b08c5865e10ee",
+    "url": "/art-app/static/media/Cinzel Regular.47b6e20b.ttf"
+  },
+  {
+    "revision": "5dad98f0d141c6bcadedd8f67131b340",
+    "url": "/art-app/static/media/Cinzel Regular.5dad98f0.eot"
+  },
+  {
+    "revision": "b3e5ef25c8d571e6b2f2aa30e93f86aa",
+    "url": "/art-app/static/media/Cinzel Regular.b3e5ef25.svg"
+  },
+  {
+    "revision": "267fdc1075e9e02e31e8267e30583ebe",
+    "url": "/art-app/static/media/Lato Black Italic.267fdc10.woff2"
+  },
+  {
+    "revision": "42a0a4028cf38dd684e0cbcc8c6b2eb3",
+    "url": "/art-app/static/media/Lato Black Italic.42a0a402.woff"
+  },
+  {
+    "revision": "8500ad891b48056629860079e7993954",
+    "url": "/art-app/static/media/Lato Black Italic.8500ad89.ttf"
+  },
+  {
+    "revision": "c2bd18fba9a5fac30e40e07e129af50f",
+    "url": "/art-app/static/media/Lato Black Italic.c2bd18fb.svg"
+  },
+  {
+    "revision": "d14976ea4e10a9d974846c858244310b",
+    "url": "/art-app/static/media/Lato Black Italic.d14976ea.eot"
+  },
+  {
+    "revision": "399e1f2a1b6fb3e0400e50c0c81f6559",
+    "url": "/art-app/static/media/Lato Black.399e1f2a.ttf"
+  },
+  {
+    "revision": "6ff48765f0d8f76804221cb5b6ed178c",
+    "url": "/art-app/static/media/Lato Black.6ff48765.woff"
+  },
+  {
+    "revision": "bd1b457a15377b2ba58e2c519ee1c16f",
+    "url": "/art-app/static/media/Lato Black.bd1b457a.woff2"
+  },
+  {
+    "revision": "c2af3cf2134a4f81adb423ccbb197b59",
+    "url": "/art-app/static/media/Lato Black.c2af3cf2.eot"
+  },
+  {
+    "revision": "c97860bdde915138d9726190424f9604",
+    "url": "/art-app/static/media/Lato Black.c97860bd.svg"
+  },
+  {
+    "revision": "315c7172013c0cb3389c2ee747b28781",
+    "url": "/art-app/static/media/Lato Bold Italic.315c7172.ttf"
+  },
+  {
+    "revision": "549191ee9b16c44b1116c34af6cba9cc",
+    "url": "/art-app/static/media/Lato Bold Italic.549191ee.eot"
+  },
+  {
+    "revision": "5ba06d4653b748ca3bbea76c1cd20a61",
+    "url": "/art-app/static/media/Lato Bold Italic.5ba06d46.svg"
+  },
+  {
+    "revision": "bb366cb67294b10c4f7cb0740c1e0726",
+    "url": "/art-app/static/media/Lato Bold Italic.bb366cb6.woff2"
+  },
+  {
+    "revision": "c5d593409de716248ca0cab4417d88a2",
+    "url": "/art-app/static/media/Lato Bold Italic.c5d59340.woff"
+  },
+  {
+    "revision": "1ea279d01a288c072e2fc63f24dc4dc3",
+    "url": "/art-app/static/media/Lato Bold.1ea279d0.woff2"
+  },
+  {
+    "revision": "bece7ff5d73a96d46a529b4309313e14",
+    "url": "/art-app/static/media/Lato Bold.bece7ff5.eot"
+  },
+  {
+    "revision": "c2094170f8da6ece9d1a6fb204d30d79",
+    "url": "/art-app/static/media/Lato Bold.c2094170.ttf"
+  },
+  {
+    "revision": "c9191174738d2d7efc7836700f74906a",
+    "url": "/art-app/static/media/Lato Bold.c9191174.woff"
+  },
+  {
+    "revision": "f619ca2684ebff2adbcbca04568ba301",
+    "url": "/art-app/static/media/Lato Bold.f619ca26.svg"
+  },
+  {
+    "revision": "0c507da97726afbc5b96703525b59283",
+    "url": "/art-app/static/media/Lato Italic.0c507da9.ttf"
+  },
+  {
+    "revision": "3589f44636a079da38c80eaa88cd5194",
+    "url": "/art-app/static/media/Lato Italic.3589f446.eot"
+  },
+  {
+    "revision": "451fc529569d1d16daf432e7c3375382",
+    "url": "/art-app/static/media/Lato Italic.451fc529.svg"
+  },
+  {
+    "revision": "6e200af00acdfcc3005f245eaafbb1f2",
+    "url": "/art-app/static/media/Lato Italic.6e200af0.woff"
+  },
+  {
+    "revision": "9fa210ba7e27c6cd869aac1f6d102b69",
+    "url": "/art-app/static/media/Lato Italic.9fa210ba.woff2"
+  },
+  {
+    "revision": "3dc55ea16acdf86c356ba60c7f19c5f6",
+    "url": "/art-app/static/media/Lato Regular.3dc55ea1.ttf"
+  },
+  {
+    "revision": "9be4d4e09d186c4f6861e4eff62577a5",
+    "url": "/art-app/static/media/Lato Regular.9be4d4e0.woff"
+  },
+  {
+    "revision": "a4545d81fc7672d34ca46b838cc90adb",
+    "url": "/art-app/static/media/Lato Regular.a4545d81.svg"
+  },
+  {
+    "revision": "b3a8aeff39163dfd02fc7f891da51c43",
+    "url": "/art-app/static/media/Lato Regular.b3a8aeff.eot"
+  },
+  {
+    "revision": "eb4d7a8908b64161591a9b01017e1637",
+    "url": "/art-app/static/media/Lato Regular.eb4d7a89.woff2"
+  },
+  {
+    "revision": "96677e466cd5a8278d8195c668838fe1",
+    "url": "/art-app/static/media/colors.96677e46.jpg"
+  },
+  {
+    "revision": "185bba9befc5ea0ad673cb0f79c7a6e7",
+    "url": "/art-app/static/media/o Light.185bba9b.svg"
+  },
+  {
+    "revision": "5bd4d468461aaac124e566c75fb01c4d",
+    "url": "/art-app/static/media/o Light.5bd4d468.woff2"
+  },
+  {
+    "revision": "a9230c0b738efccb4f31a2e2a08e2570",
+    "url": "/art-app/static/media/o Light.a9230c0b.woff"
+  },
+  {
+    "revision": "ca908c325f9ab1de0c910c1d291fa50a",
+    "url": "/art-app/static/media/o Light.ca908c32.eot"
+  },
+  {
+    "revision": "f96abc9c63c820827da0946adeb506b8",
+    "url": "/art-app/static/media/o Light.f96abc9c.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24701d088fc5c09b0935382c07b3de93.js"
+  "/art-app/precache-manifest.e43743725768fa5d3d182c0164deeb89.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/art-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
